359cd7b21fd2daeb97bc6bf1806d06fdf46d0ecd
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-09-28 13:54:32 -0400

    finished v1 stuff with some small styling with bootstrapbut no mongoose-mongo

--- app.js
+++ app.js
@@ -1,10 +1,70 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true })); //use all the time cut paste
 app.set("view engine", "ejs");
 
+var campgrounds = [
+  {
+    name: "Salmon Creek",
+    image: "https://via.placeholder.com/350x150"
+  },
+  {
+    name: "Granite Hill",
+    image: "https://via.placeholder.com/350x150"
+  },
+  {
+    name: "Mountain  Goat's Rest",
+    image: "https://via.placeholder.com/350x150"
+  },
+  {
+    name: "Salmon Creek",
+    image: "https://via.placeholder.com/350x150"
+  },
+  {
+    name: "Granite Hill",
+    image: "https://via.placeholder.com/350x150"
+  },
+  {
+    name: "Mountain  Goat's Rest",
+    image: "https://via.placeholder.com/350x150"
+  },
+  {
+    name: "Salmon Creek",
+    image: "https://via.placeholder.com/350x150"
+  },
+  {
+    name: "Granite Hill",
+    image: "https://via.placeholder.com/350x150"
+  },
+  {
+    name: "Mountain  Goat's Rest",
+    image: "https://via.placeholder.com/350x150"
+  }
+];
+
 app.get("/", function(req, res) {
-  console.log("workin!");
+  res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res) {
+  res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+app.post("/campgrounds", function(req, res) {
+  //get data from form and add to campgrounds array
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = { name: name, image: image };
+  campgrounds.push(newCampground);
+  //redirect back to campgroundspage
+  res.redirect("campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new.ejs");
 });
 
 app.listen(4000, function() {
